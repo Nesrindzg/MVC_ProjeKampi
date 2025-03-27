b805b3ea73bdd8a440aec1c42edb666710fd8c74
Nesrin Düzgün <nduzgun10@gmail.com>
2025-03-27 16:33:20 +0300

    40.Ders

---
+++ BusinessLayer/Abstract/ICategoryService.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface ICategoryService
+    {
+        List<Category> GetList();
+        void CategoryAdd(Category category);
+
+        Category GetByID(int id);
+
+        void CategoryDelete(Category category); // Bu parametre ile Category sınıfındaki tüm alanlara ulaşabileceğiz.
+
+        void CategoryUpdate(Category category);
+    }
+}
---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,71 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repostories;
+using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDAL _categoryDAL; //Veritabanımı temsil eden interfaceler DAL katmanında yer alıyor.
+
+        public CategoryManager(ICategoryDAL categoryDAL)
+        {
+            _categoryDAL = categoryDAL;
+        }
+
+        public void CategoryAdd(Category category)
+        {
+            _categoryDAL.Insert(category);
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            _categoryDAL.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            _categoryDAL.Update(category); 
+        }
+
+        public Category GetByID(int id)
+        {
+            return _categoryDAL.Get(x=> x.CategoryID==id);
+        }
+
+        public List<Category> GetList()
+        {
+            return _categoryDAL.List();
+        }
+
+        // Kullanmıyoruz. Bunun yerine fluent validation(Business da ValidationRules)
+        //GenericRepository<Category> repo= new GenericRepository<Category>(); // Data Access katmanına erişim
+
+        //public List<Category> GetAllBL()
+        //{
+        //    return repo.List();
+        //}
+
+        //public void CategoryAddBL(Category p)
+        //{
+        //    /*if (p.CategoryName=="" || p.CategoryName.Length<=3)
+        //    {
+        //        // Hata mesajı yazdır.
+        //    }
+        //    else
+        //    {
+        //        // Koşullar tamamsa ekleme işlemini burda yaptırıyoruz.
+        //        repo.Insert(p);
+        //    }*/
+
+        //    repo.Insert(p);
+        //}
+
+    }
+}
