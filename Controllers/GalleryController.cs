adfdb3fc6522b993df8a06f21f55422399a2880f
Nesrin Düzgün <nduzgun10@gmail.com>
2025-04-16 17:59:07 +0300

    Gallery Page

---
+++ Controllers/GalleryController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_ProjeKampi.Controllers
+{
+    public class GalleryController : Controller
+    {
+        ImageFileManager ifm = new ImageFileManager(new EFImageFileDAL());
+        public ActionResult Index()
+        {
+            var values = ifm.GetList();
+            return View(values);
+        }
+    }
+}
