6f35110365a9b3459544e363d9fd9f26260d1654
Nesrin Düzgün <nduzgun10@gmail.com>
2025-04-30 12:31:29 +0300

    Google Charts ekleme - grafikler

---
+++ Controllers/ChartController.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using MVC_ProjeKampi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_ProjeKampi.Controllers
+{
+    public class ChartController : Controller
+    {
+        private readonly ChartService _chartService = new ChartService();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult BlogChart()
+        {
+            var data = _chartService.GetBlogList();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ColumnChart()
+        {
+            return View();
+        }
+
+        public ActionResult ContentListChart()
+        {
+            var data = _chartService.GetContentList();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult LineChart()
+        {
+            return View();
+        }
+
+        public ActionResult WriterListChart()
+        {
+            var data = _chartService.GetWriterList();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+}
---
+++ Models/ChartService.cs
@@ -0,0 +1,58 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_ProjeKampi.Models
+{
+    public class ChartService
+    {
+        private readonly HeadingManager _headingManager;
+        private readonly ContentManager _contentManager;
+
+        public ChartService()
+        {
+            _headingManager = new HeadingManager(new EFHeadingDAL());
+            _contentManager = new ContentManager(new EFContentDAL());
+        }
+
+        public List<CategoryChart> GetContentList()
+        {
+            return _contentManager.GetList("")
+                .Where(w => w.ContentStatus)
+                .GroupBy(x => x.Heading.Category.CategoryName)
+                .Select(x => new CategoryChart
+                {
+                    CategoryName = x.Key,
+                    CategoryCount = x.Count()
+                }).ToList();
+        }
+
+        public List<CategoryChart> GetBlogList()
+        {
+            return _headingManager.GetList()
+                .Where(w => w.HeadingStatus)
+                .GroupBy(x => x.Category.CategoryName)
+                .Select(x => new CategoryChart
+                {
+                    CategoryName = x.Key,
+                    CategoryCount = x.Count()
+                }).ToList();
+        }
+
+        public List<CategoryChart> GetWriterList()
+        {
+            return _contentManager.GetList("")
+                .Where(w => w.ContentStatus)
+                .GroupBy(x => x.Heading.Category.CategoryName)
+                .Select(x => new CategoryChart
+                {
+                    CategoryName = x.Key,
+                    CategoryCount = x.Count()
+                }).ToList();
+        }
+    }
+
+}
